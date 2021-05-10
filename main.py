46bbb6374617403e286b25044249590e081e9d5e
jvloiola <77701962+jvloiola@users.noreply.github.com>
2021-05-10 17:29:54 -0300

    Update main.py

--- main.py
+++ main.py
@@ -15,18 +15,19 @@ def execute(sql, params=None):
         with conn.cursor() as cursor: # abre uma página para executar coisas
             cursor.execute(sql, params) # executa o sql que está sendo passado por parametro
             conn.commit() # grava as coisas no banco de dados
+            return cursor.lastrowid
 
 def query(sql, params=None):
     """ Executa um comando no mysql e retorna o resultado
         Serve para: Select, SHOW """
     with connect(**CONFIGURACOES_BD) as conn: #conecta no banco
-        with conn.cursor() as cursor: # abre uma página para executar coisas
+        with conn.cursor(dictionary=True) as cursor: # abre uma página para executar coisas
             cursor.execute(sql, params) # executa o sql que está sendo passado por parametro
             return cursor.fetchall() # pega o resultado da consulta e retorna
 
 
 def insert(tabela, colunas, valores):
-    execute(f"INSERT INTO {tabela} ({','.join(colunas)}) VALUES ({','.join(['%s' for valor in valores])})", valores)
+    return execute(f"INSERT INTO {tabela} ({','.join(colunas)}) VALUES ({','.join(['%s' for valor in valores])})", valores)
 
 
 def delete(tabela, coluna, valor):
@@ -41,7 +42,6 @@ def update(tabela, chave, valor_chave, colunas, valores):
 def select(tabela, chave=1, valor_chave=1, limit=100, offset=0):
     return query(f"""SELECT * FROM {tabela} WHERE {chave} = %s LIMIT {limit} offset {offset}""", (valor_chave,))
 
-select("estados")
-# delete("idiomas", "id", 152)
-# insert("idiomas", ["sigla", "nome"], ["ZZ", "Big Brother Brasil"])
-# update("idiomas", "id", 152, ["sigla", "nome"], ["ZY", "Big Brother Curitiba"])
+
+def select_like(tabela, chave=1, valor_chave=1, limit=100, offset=0):
+    return query(f"""SELECT * FROM {tabela} WHERE {chave} LIKE %s LIMIT {limit} offset {offset}""", (f"%{valor_chave}%",))
