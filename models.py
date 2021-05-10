d4d79c06bb14e516bf266e194bb7858f69732413
jvloiola <77701962+jvloiola@users.noreply.github.com>
2021-05-10 17:30:18 -0300

    Update models.py

--- models.py
+++ models.py
@@ -1,54 +1,66 @@
-from main import insert, select, update, delete
+from main import *
 
+#--------------------------#USUARIO#--------------------------#
+def insert_usuario(nome_completo, CPF):
+    return insert("usuarios", ["nome_completo", "CPF"], [nome_completo, CPF])
 
-"""_________________CRUD DIRETORES________"""
-def inserir_diretores (nome_completo):
-    insert("diretores", ["nome_completo"],[nome_completo])
+def get_usuario(id):
+    return select("usuarios", "id", id)[0]
 
-def alterar_diretores (id):
-    update("diretores","id",id,"nome_completo")
+def update_usuario(id, nome_completo, CPF):
+    update("usuarios", "id", id, ["nome_completo", "CPF"], [nome_completo, CPF])
 
-def apagar_diretores (id):
-    delete ("diretores","id",id)
+def delete_usuario(id):
+    delete("usuarios", "id", id)
 
-def buscar_diretores (id):
-    select("diretores","id",id)
+def select_usuarios(nome_completo):
+    return select_like("usuarios", "nome_completo", nome_completo)
 
-"""________________CRUD GENEROS_________"""
-def inserir_generos (id):
-    insert("generos", ["nome"],[nome])
+#--------------------------#FILME#--------------------------#
+def insert_filme(id):
+    return insert("filmes", ["titulo", "ano", "classificacao", "preco", "diretores_id", "generos_id"], 
+    [titulo, ano, classificacao, preco, diretores_id, generos_id])]
 
-def alterar_generos (id):
-    update("generos","id",id,"nome")
+def get_filme(id):
+    return select ("filmes","id", id)[0]
 
-def apagar_generos (id):
-    delete ("generos","id",id)
+def update("filmes","id",["titulo", "ano", "classificacao", "preco", "diretores_id", "generos_id"],
+    [titulo, ano, classificacao, preco, diretores_id, generos_id])
 
-def buscar_generos (id):
-    select("generos","id",id)
+def delete_filme(id):
+    delete("filmes", "id", id)
 
-"""______________CRUD FILMES____________"""
-def inserir_filmes (id):
-    insert("filmes","colunas","valores")
+def select_filmes(titulo):
+    return select_like("filmes","titulo", titulo)
 
-def alterar_filmes (id):
-    update("filmes","id",id,"nome")
+#--------------------------#GENERO#--------------------------#
+def insert_genero(nome):
+    return insert("genero", ["nome"], [nome])
 
-def apagar_filmes (id):
-    delete ("filmes","id",id)
+def get_genero(id):
+    return select("genero", "id", id)[0]
 
-def buscar_filmes ():
-    select("filmes","id",id)
+def update_genero(id, nome):
+    update("genero", "id", id, ["nome"], [nome])
 
-"""______________CRUD USUARIO___________"""
-def inserir_usuario (nome_completo):
-    insert("usuario","colunas","valores")
+def delete_genero(id):
+    delete("genero", "id", id)
 
-def alterar_usuario (id):
-    update("usuario","id",id,"nome")
+def select_genero(nome):
+    return select_like("genero", "nome", nome)
 
-def apagar_usuario (id):
-    delete ("usuario","id",id)
+#--------------------------#DIRETORES#--------------------------#
+def insert_diretores(nome_completo, id):
+    return insert("usuarios", ["nome_completo", "id"], [nome_completo, id])
 
-def buscar_usuario ():
-    select("usuario","id",id)
+def get_diretores(id):
+    return select("usuarios", "id", id)[0]
+
+def update_diretores(id, nome_completo, id):
+    update("usuarios", "id", id, ["nome_completo", "id"], [nome_completo, id])
+
+def delete_diretores(id):
+    delete("usuarios", "id", id)
+
+def select_diretores(nome_completo):
+    return select_like("usuarios", "nome_completo", nome_completo)
